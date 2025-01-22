447994e9ca4f7432df8ca3bd0d3e9a839be6a12c
GBarbosa <andradegui2006@gmail.com>
2025-01-22 15:45:01 -0300

    JS

--- JavaScript/Lucian's Luscious Lasagna/main.js
+++ JavaScript/Lucian's Luscious Lasagna/main.js
@@ -3,18 +3,17 @@ export const EXPECTED_MINUTES_IN_OVEN = 40;
 
 export function remainingMinutesInOven(actualMinutesInOven) 
 {
-  return (EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven);
+    return (EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven);
 }
 
 export function preparationTimeInMinutes(numberOfLayers) 
 {
-  return (numberOfLayers * 2);
+    return (numberOfLayers * 2);
 }
 
-export function totalTimeInMinutes(numberOfLayers, actualMinutesInOven) 
+export function totalTimeInMinutes(numberOfLayers, actualMinutesInOven)
 {
-    //throw new Error('Remove this line and implement the function');
     let timePreparing;
     timePreparing = preparationTimeInMinutes(numberOfLayers);
     return timePreparing + actualMinutesInOven;
-  }
+}
