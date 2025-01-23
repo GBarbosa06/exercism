1f60bf5051940be6bd2683c9120311f8be0ec263
GBarbosa <andradegui2006@gmail.com>
2025-01-23 16:57:13 -0300

    Create main.c

---
+++ C/Grains/Collatz Conjecture/main.c
@@ -0,0 +1,25 @@
+#include "collatz_conjecture.h"
+
+int steps(int start)
+{
+    if(start < 1)
+    {
+        return -1;
+    }
+    int c = 0;
+     while (start > 1)
+     {
+        if(start%2 == 0)
+        {
+            start /= 2;            
+        }
+         else
+         {
+             start = start * 3 + 1;
+         }
+
+
+         c++;
+     }
+    return c;
+}
