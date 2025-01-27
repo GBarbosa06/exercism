18a59a19ad38f6674e6182c5ecba6561ac55b5d9
GBarbosa <andradegui2006@gmail.com>
2025-01-27 20:49:03 -0300

    complete

--- C/Start Darts/main.c
+++ C/Start Darts/main.c
@@ -1,1 +1,30 @@
 #include "darts.h"
+
+
+int score(coordinate_t landing_position)
+{
+    float D;
+    float x = landing_position.x;
+    float y = landing_position.y;
+    D = sqrt(x*x + y*y);
+
+    if (D <= 1) 
+    {
+        return 10;
+    }
+    else if (D <= 5)
+    {
+        return 5;
+    }
+    else if (D <= 10)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+    
+    
+    
+}
