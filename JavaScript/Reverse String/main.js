892db6b4efbd6ea6f4ca621886439d1b10824518
GBarbosa <andradegui2006@gmail.com>
2025-01-31 19:31:34 -0300

    Create main.js

---
+++ JavaScript/Reverse String/main.js
@@ -0,0 +1,8 @@
+export const reverseString = (str) => {
+    let reversedStr = '';
+    for(let i = 0; i < str.length; i++){
+        reversedStr += str[str.length-i-1];
+    }
+    return reversedStr;
+  };
+  
