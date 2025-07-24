aa527a64d0f23c75cf0253b825e372ee204b45c4
gbarbosa06 <andradegui2006@gmail.com>
2025-07-23 22:27:17 -0300

    Update RnaTranscription.java

--- Java/src/RNATranscription/RnaTranscription.java
+++ Java/src/RNATranscription/RnaTranscription.java
@@ -3,13 +3,11 @@ public class RnaTranscription {
 
         dnaStrand = dnaStrand.replace('G', 'c' );
         dnaStrand = dnaStrand.replace('C', 'g');
-        dnaStrand = dnaStrand.replace('T', 'a');
         dnaStrand = dnaStrand.replace('A', 'U');
+        dnaStrand = dnaStrand.replace('T', 'A');
 
         dnaStrand = dnaStrand.replace('c', 'C');
         dnaStrand = dnaStrand.replace('g', 'G');
-        dnaStrand = dnaStrand.replace('a', 'A');
-
         return dnaStrand;
     }
 }
